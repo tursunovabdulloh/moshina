cdd9ce168cfa5d1761ea0b4db8f132ea54f3957f
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-04-18 10:43:47 +0300

    1515

--- app.js
+++ app.js
@@ -7,65 +7,49 @@ const colorInput = document.getElementById("color");
 const submitButton = document.getElementById("submit");
 const carlist = document.getElementById("carlist");
 
+let formArr = JSON.parse(localStorage.getItem("arr"))
+  ? JSON.parse(localStorage.getItem("arr"))
+  : [];
+creatForm();
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const ism = nameInput.value;
-  const tezlik = speedInput.value;
-  const narx = priceInput.value;
-  const imgurl = imgUrlInput.value;
-  const rang = colorInput.value;
-
-  const nameli = document.createElement("li");
-  nameli.classList.add("ismform");
-  nameli.textContent = `${ism}`;
-
-  const speedli = document.createElement("li");
-  speedli.classList.add("speedform");
-  speedli.textContent = `Speed: ${tezlik}`;
-
-  const priceli = document.createElement("li");
-  priceli.classList.add("priceform");
-  priceli.textContent = `Price: ${narx}`;
-
-  const imgUrlli = document.createElement("img");
-  imgUrlli.classList.add("imgform");
-  imgUrlli.setAttribute("src", imgurl);
-  imgUrlli.setAttribute("alt", ism);
-
-  const colorli = document.createElement("div");
-  colorli.classList.add("colorform");
-  colorli.textContent = `Color: `;
-  colorli.style.backgroundColor = rang;
-  colorli.style.width = "100px";
-  colorli.style.height = "20px";
-
-  const kerak = document.createElement("div");
-  kerak.classList.add("boreder");
-  kerak.appendChild(nameli);
-  kerak.appendChild(speedli);
-  kerak.appendChild(priceli);
-  kerak.appendChild(colorli);
-  kerak.appendChild(imgUrlli);
-  carlist.appendChild(kerak);
-
-  nameInput.value = "";
-  speedInput.value = "";
-  priceInput.value = "";
-  imgUrlInput.value =
-    "https://img.razrisyika.ru/kart/57/1200/225582-bugatti-chiron-24.jpg";
-
-  const list = document.createElement("div");
-  list.classList.add("car");
-  list.appendChild(imgUrlli);
-  list.appendChild(nameli);
-  list.appendChild(speedli);
-  list.appendChild(priceli);
-  list.appendChild(colorli);
-  carlist.appendChild(list);
-
-  nameInput.value = "";
-  speedInput.value = "";
-  priceInput.value = "";
-  imgUrlInput.value =
-    "https://img.razrisyika.ru/kart/57/1200/225582-bugatti-chiron-24.jpg";
+  let hisob = {
+    id: Math.floor(Math.random() * 100000),
+    imgUrl: imgUrlInput.value,
+    name: nameInput.value,
+    speed: speedInput.value,
+    price: priceInput.value,
+    color: colorInput.value,
+  };
+  formArr.push(hisob);
+  localStorage.setItem("arr", JSON.stringify(formArr));
+  creatForm();
 });
+
+function creatForm(data = formArr) {
+  carlist.innerHTML = "";
+  data.forEach(({ name, speed, price, imgUrl, color, id }) => {
+    const li = document.createElement("li");
+    li.classList.add("card");
+    carlist.innerHTML += `
+    <div class="li1_div">
+    <img class="moshina_rasm" src="${imgUrl}" alt=""/>
+    <div class="li2_div">
+    <h2 class="ism">${name}</h2>
+    <p class="speed">speed: ${speed}</p>
+    <p class="price">price: ${price}</p>
+    <p class="color">color: ${color} </p>
+    <i class="fa-solid fa-trash-can trash-png" style="color: #3b9feb" onclick="delateForm(${id})"></i>
+    </div>  
+    </div>
+    `;
+    carlist.appendChild(li);
+  });
+}
+
+function delateForm(itemId) {
+  formArr = formArr.filter(({ id }) => id !== itemId);
+  localStorage.setItem("arr", JSON.stringify(formArr));
+  creatForm(formArr);
+}
